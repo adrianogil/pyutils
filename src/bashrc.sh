e8aac4ad202420b7130829032099c2f40eb3e23c
Adriano Gil <adrianogil.san@gmail.com>
2021-01-30 15:05:42 -0400

    Minor refactoring of folder structure

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -52,9 +52,9 @@ function py-which()
 
 if [ -z "$PYTOOLS_PYTHON_PATH" ];
 then
-    export PYTOOLS_PYTHON_PATH=$PYTOOLS_DIR/python/
+    export PYTOOLS_PYTHON_PATH=$PYTOOLS_DIR/
     export PYTHONPATH=$PYTOOLS_PYTHON_PATH:$PYTHONPATH
 fi
 
-alias py-pickle-viewer="python3 $PYTOOLS_DIR/python/pyutils/pickle_view.py"
-alias py-json-load="python3 -i $PYTOOLS_DIR/python/pyutils/json_load.py"
+alias py-pickle-viewer="python3 $PYTOOLS_DIR/pyutils/pickle_view.py"
+alias py-json-load="python3 -i $PYTOOLS_DIR/pyutils/json_load.py"
