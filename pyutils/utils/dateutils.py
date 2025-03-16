b3b2d90d4a2ecda30d8c2477eeb27e4f02f1cc85
Adriano Gil <adrianogil.san@gmail.com>
2025-03-16 00:14:05 -0400

    feat: enhance get_datetime to support ISO date format

--- pyutils/utils/dateutils.py
+++ pyutils/utils/dateutils.py
@@ -2,6 +2,8 @@ from datetime import datetime, date
 
 def get_datetime(date_value):
     if isinstance(date_value, str):
+        if "-" in date_value:
+            return datetime.strptime(date_value, '%Y-%m-%d')
         return datetime.strptime(date_value, '%Y.%m.%d')
     elif isinstance(date_value, date):
         return datetime(date_value.year, date_value.month, date_value.day)
